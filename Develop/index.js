a2f48eddc473156bcb144ad5d10b4d991d9f8c78
Connie Tran <connietrandev@gmail.com>
2020-04-29 15:49:22 -0400

    Add GitHub API call

--- Develop/index.js
+++ Develop/index.js
@@ -1,63 +1,76 @@
 const inquirer = require('inquirer');
 
+const api = require('./utils/api.js');
+
 const questions = [
     {
         type: 'input',
         message: "What is your GitHub username? (No @ needed)",
-        name: 'username'
-        // validation 
-      },
+        name: 'username',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid GitHub username.");
+            }
+            return true;
+        }
+    },
       {
         type: 'input',
         message: "What is the title of your project?",
-        name: 'title'
-      },
-      {
-        type: 'input',
-        message: "Write a description of your project.",
-        name: 'description'
-      },
-      {
-        type: 'input',
-        message: "Describe the steps required to install your project for the Installation section.",
-        name: "installation"
-      },
-      {
-        type: 'input',
-        message: "Provide instructions and examples of your project in use for the Usage section.",
-        name: 'usage'
-      },
-      {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
-      },
-      {
-        type: 'input',
-        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
-        name: 'contributing'
-      },
-      {
-        type: 'input',
-        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
-        name: 'tests'
-      },
+        name: 'title',
+        default: 'Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid project description.");
+            }
+            return true;
+        }
+    },
+    //   {
+    //     type: 'input',
+    //     message: "Write a description of your project.",
+    //     name: 'description'
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: "Describe the steps required to install your project for the Installation section.",
+    //     name: "installation"
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: "Provide instructions and examples of your project in use for the Usage section.",
+    //     name: 'usage'
+    //   },
+    //   {
+    //     type: 'list',
+    //     message: "Choose a license for your project.",
+    //     choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    //     name: 'license'
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+    //     name: 'contributing'
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+    //     name: 'tests'
+    //   },
 ];
 
 function writeToFile(fileName, data) {
 }
 
 async function init() {
-    await inquirer
-        .prompt(questions)
-        .then(response => {
-            console.log(response);
-            console.log("Thank you for your responses! Fetching your GitHub data next...");
-        });
+    const userResponses = await inquirer.prompt(questions);
 
-    await console.log('next');
-    
+    console.log("Your responses: ", userResponses);
+    console.log("Thank you for your responses! Fetching your GitHub data next...");
+
+    const userInfo = await api.apiCalls.getUser(userResponses);
+
 }
 
 init();
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,14 @@
-const api = {
-  getUser(username) {
+const axios = require('axios');
 
-  }
+const api = {
+  async getUser(userResponses) {
+    let response = await axios
+      // Sample URL: https://api.github.com/users/connietran-dev
+      .get(`https://api.github.com/users/${userResponses.username}`);
+      return response.data;
+    }
 };
 
-module.exports = api;
+module.exports = {
+  apiCalls: api
+};
